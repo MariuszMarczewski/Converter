0a745070f9fb88a2ea15d21179555bda4b69926b
Mariusz Marczewski <mariusz.marczewski1@poczta.fm>
2019-10-12 19:49:11 +0200

    Final

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -5,8 +5,8 @@ import pl.sda.converter.FileConverter;
 public class App {
     public static void main(String[] args) {
         FileConverter converter = new FileConverter();
-        String sourceFilePath = "/home/michal/SDA/Java19/Projekty/converter/src/main/resources/test.csv";
-        String outputFilePath = "/home/michal/SDA/Java19/Projekty/converter/src/main/resources/testOut.json";
+        String sourceFilePath = "C:\\Users\\lenovo\\IdeaProjects\\SDA_J19_Converter\\src\\main\\resources\\testOut.xlsx";
+        String outputFilePath = "C:\\Users\\lenovo\\IdeaProjects\\SDA_J19_Converter\\src\\main\\resources\\testOut2.json";
         converter.convert(sourceFilePath, outputFilePath);
     }
 }
---
+++ src/main/java/pl/sda/converter/excell/ExcellWriter.java
@@ -0,0 +1,63 @@
+package pl.sda.converter.excell;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import pl.sda.converter.SDAFileWriter;
+import pl.sda.converter.exceptions.FileConverterException;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+class ExcelWriter implements SDAFileWriter {
+
+    @Override
+    public void write(List<Map<String, Object>> records, String path) {
+        Workbook workbook = new XSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet();
+
+        //1.Tworzenie wiersza nagłówkowego
+        Set<String> headers = new HashSet<>();
+        for (Map<String, Object> record : records) {
+            headers.addAll(record.keySet());
+        }
+        Row headerRow = sheet.createRow(0);
+        int cellCounter = 0;
+        for (String header : headers) {
+            Cell cell = headerRow.createCell(cellCounter);
+            cell.setCellValue(header);
+            cellCounter++;
+        }
+
+        //2. Generowanie wierszy z danymi
+        int rowCounter = 1;
+        for (Map<String, Object> record : records) {
+            Row row = sheet.createRow(rowCounter);
+            cellCounter = 0;
+            for (String header : headers) {
+                Cell cell = row.createCell(cellCounter);
+                cell.setCellValue(record.get(header).toString());
+                cellCounter++;
+            }
+            rowCounter++;
+        }
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(path);
+            workbook.write(fileOutputStream);
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            throw new FileConverterException("Nie odnaleziono pliku", e);
+        } catch (IOException e) {
+            throw new FileConverterException("Błąd podczas generowania pliku", e);
+        }
+    }
+}
